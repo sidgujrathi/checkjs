4235845cce291a9c5bf16f4b58d8cf9916c9610e
sidgujrathi <sidh.gujrathi@gmail.com>
2016-05-18 21:51:59 +0530

    error messeges for validation initialization

--- checkjs.js
+++ checkjs.js
@@ -12,17 +12,28 @@
         	urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
         	usPhoneRegex = /^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$ | [0-9]{3} [0-9]{3}-[0-9]{4}$/,
         	usZipcodeRegex = /^\d{5}(?:[-\s]\d{4})?$/,
-        	creditCardRegex = /^(?:4[0-9]{12}(?:[0-9]{3})? |  5[1-5][0-9]{14}|  3[47][0-9]{13}|  3(?:0[0-5]|[68][0-9])[0-9]{11}| 6(?:011|5[0-9]{2})[0-9]{12} | (?:2131|1800|35\d{3})\d{11})$/;
+        	creditCardRegex = /^(?:4[0-9]{12}(?:[0-9]{3})? |  5[1-5][0-9]{14}|  3[47][0-9]{13}|  3(?:0[0-5]|[68][0-9])[0-9]{11}| 6(?:011|5[0-9]{2})[0-9]{12} | (?:2131|1800|35\d{3})\d{11}) | \d{16} $/;
+ 		
+ 		/*Validation error messeges*/
+ 		var messeges = {
+
+ 						invalidUrl:"Url you have entered is in wrong formate",
+ 						invalidNumber:"",
+ 						invalidEmail:"",
+ 						invalidPhone:"",
+ 						invalidZipCode:"",
+ 						invalidCreditCard:""
+ 		};
  			
 			
 	}
 
-	if(typeof(Postfeed) === 'undefined') {
-   	    window.Postfeed = define_Checkjs();
+	if(typeof(Checkjs) === 'undefined') {
+   	    window.Checkjs = define_Checkjs();
 
     }
     else {
-        console.log("Postfeed already defined.");
+        console.log("Checkjs already defined.");
     }
 
     
