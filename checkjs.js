d891870a86e692fe831c9d8290f215f1aabc5f2f
sidgujrathi <sidh.gujrathi@gmail.com>
2016-05-18 19:55:53 +0530

    library skeleton adn intialization

---
+++ checkjs.js
@@ -0,0 +1,30 @@
+(function(window)
+{
+	'use strict';
+	/*Initialization of checkjs in case of Undefined*/
+	function define_Checkjs() {
+		var Checkjs={};
+		
+		/*Define regular expression rules*/	
+		
+		var numericRegex = /^[0-9]+$/,
+            emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        	urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
+        	usPhoneRegex = /^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$ | [0-9]{3} [0-9]{3}-[0-9]{4}$,
+        	usZipcodeRegex = /^\d{5}(?:[-\s]\d{4})?$,
+        	creditCardRegex = /^(?:4[0-9]{12}(?:[0-9]{3})? |  5[1-5][0-9]{14}|  3[47][0-9]{13}|  3(?:0[0-5]|[68][0-9])[0-9]{11}| 6(?:011|5[0-9]{2})[0-9]{12} | (?:2131|1800|35\d{3})\d{11})$;
+ 			
+			
+	}
+
+	if(typeof(Postfeed) === 'undefined') {
+   	    window.Postfeed = define_Checkjs();
+
+    }
+    else {
+        console.log("Postfeed already defined.");
+    }
+
+    
+	
+})(window);
